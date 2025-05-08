ed21a95fbb24535b7b7c9345f1131641f3028dca
Julio-dev-e <julio.luengo@hpecds.com>
2025-05-08 16:29:47 +0200

    commit

--- Front-End/src/app/home/home.component.ts
+++ Front-End/src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public currentUser;
+  public loading: boolean = false;
+
   constructor() {
     this.currentUser = localStorage.getItem('currentUser')? JSON.parse(localStorage.getItem('currentUser')) : '';
    }
